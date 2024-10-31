ec93cb2128217041b21cab56f584a83fde600ef9
Mirstacks <ssmith876@boyslatin.net>
2024-10-31 12:50:18 +0000

    Started working on Python

---
+++ Unit 3 Python/pythonFoundation.py
@@ -0,0 +1,30 @@
+# data types = are fundamental
+# blocks of python
+
+# intergers = whole numbers
+# strings = any character wrapped in quotes
+# boolean = true or false
+# we output data using the print function
+print(200 * 80)
+print('Welcome to python!')
+print(true)
+
+() #round brackets = function
+{} #curly brackets = objects
+[] #square brackets = lists
+
+name = 'Samir'
+message = 'Good morning,'
+
+# concatenation = combining strings
+print(message, name)
+
+story = "I will be going to"
+location = 'Mr. Golden\'s room'
+time = '@ 8:46AM'
+print(story, location, time)
+
+valA=10
+valB=20
+
+print(valA + location)

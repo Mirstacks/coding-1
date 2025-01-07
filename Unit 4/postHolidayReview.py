cf974d5936f5e46178b221fc8fc71063c6c54401
Mirstacks <ssmith876@boyslatin.net>
2025-01-07 13:46:24 +0000

    Holiday Review

--- Unit 4/postHolidayReview.py
+++ Unit 4/postHolidayReview.py
@@ -2,10 +2,10 @@
 # (numbers, letters, true or false)
 
 # Operators - Level 2: The ability to manipulate and do things
-# with data types (math, comparisions, assignment, etc.)
+# with data types. (math, comparisions, assignment, etc.)
 
 # Functions - Level 3: Taking the first two concepts and organizing 
-# these operations and data types into instructions 
+# these operations and data types into instructions.
 
 # Conditional Statements - Level 3a: Being able to add more control 
-# on our function instructions 
+# on our function instructions.
